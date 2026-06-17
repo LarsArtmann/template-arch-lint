4dfdd00eb1efef10061ec4b94eac9d66deef5fda
Lars Artmann <git@lars.software>
2026-06-17 22:48:13 +0200

    chore: remove justfile(s), add/update Nix flake, update Go dependencies
    Removed justfile(s) (migrated to Nix flakes):
      - justfile
      - template-configs/justfile
    Nix flake configuration:
      - flake.lock
      - flake.nix
    Go module dependencies:
      - go.sum
    
    Diffstat: 5 files changed, 167 insertions(+), 3128 deletions(-)

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "Architecture linter template for Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      systems,
+      treefmt-nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          treefmt = {
+            projectRootFile = "go.mod";
+            programs = {
+              gofumpt.enable = true;
+              goimports.enable = true;
+              nixfmt.enable = true;
+              templ.enable = true;
+            };
+          };
+
+          checks.format = config.treefmt.build.check self;
+
+          devShells = {
+            default = pkgs.mkShell {
+              name = "template-arch-lint-dev";
+
+              packages = [
+                pkgs.go_1_26
+                pkgs.golangci-lint
+                pkgs.gopls
+                pkgs.delve
+                pkgs.gotools
+                pkgs.gofumpt
+                pkgs.templ
+              ];
+
+              GOWORK = "off";
+              GOPRIVATE = "github.com/LarsArtmann/*,github.com/larsartmann/*";
+            };
+
+            ci = pkgs.mkShellNoCC {
+              packages = [
+                pkgs.go_1_26
+                pkgs.golangci-lint
+                pkgs.templ
+              ];
+
+              GOWORK = "off";
+              GOPRIVATE = "github.com/LarsArtmann/*,github.com/larsartmann/*";
+            };
+          };
+        };
+    };
+}
